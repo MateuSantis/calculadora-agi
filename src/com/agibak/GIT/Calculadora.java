36b16781beb63a07ae1d472091bd587fde3c9efa
mateus.santis <mateusantis2@gmail.com>
2025-02-18 09:08:22 -0300

    Adicionando calculadora, primeira aula de GIT Agi

---
+++ src/com/agibak/GIT/Calculadora.java
@@ -0,0 +1,11 @@
+package com.agibak.GIT;
+
+public class Calculadora {
+    public static int soma(int a, int b){
+        return a+b;
+    }
+
+    public static void main(String[] args){
+        System.out.println("Resultado " + soma(5,3));
+    }
+}
