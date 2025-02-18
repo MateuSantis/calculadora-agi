86fee73e6806f45f11db511dabbc5032e417b06c
mateus.santis <mateusantis2@gmail.com>
2025-02-18 09:39:49 -0300

    Adicionando a conta de "-"

--- src/com/agibak/GIT/Calculadora.java
+++ src/com/agibak/GIT/Calculadora.java
@@ -5,7 +5,12 @@ public class Calculadora {
         return a+b;
     }
 
+    public static int subtracao(int a, int b){
+        return a-b;
+    }
+
     public static void main(String[] args){
         System.out.println("Resultado " + soma(5,3));
+        System.out.println("Resultado da subtração: " + subtracao(5, 3));
     }
 }
